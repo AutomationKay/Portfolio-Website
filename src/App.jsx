ad7a4d90ed165aa2de96224f9d1cca6cd1704463
Kamaal Bartlett <112325655+AutomationKay@users.noreply.github.com>
2025-06-13 10:04:04 -0400

    Display configuration updates

--- src/App.jsx
+++ src/App.jsx
@@ -56,7 +56,7 @@ export default function App() {
         <>
           <Background />
           <Navbar />
-          <div className="pt-28 px-4 md:px-8 max-w-screen-xl mx-auto min-h-screen">
+          <div className="flex flex-col justify-between w-full min-h-screen px-4 md:px-8">
             <AnimatePresence mode="wait">
               <Routes>
                 <Route path="/" element={<Home />} />
