4c91a972af0eea0bf47eb7dbf652494af772d303
AutomationKay <112325655+AutomationKay@users.noreply.github.com>
2025-06-16 12:25:54 -0400

    Updates

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -15,12 +15,18 @@ export default function About() {
         />
 
         <p className="text-xl leading-relaxed text-white text-center md:text-left">
-          I’m Kamaal Bartlett — Software Engineer, Robotics Enthusiast, and Data Science masters graduate.
-          I began my career working in Accounting and Finance before taking the leap to dive into the world of Computer Science.
-          Since making this decision I've spent countless hours in preparation to make this career change.
-          I completed a Data Science bootcamp from Springboard, attended and graduated from The University of Denver with a Master's degree in Computer Science,
+          I’m Kamaal Bartlett — Software Engineer, Robotics Enthusiast, and Data Science Master's graduate.
+                  I started out in Accounting and Finance, but my passion for experimenting with technology and problem-solving led me to dive into Computer Science.
 
-          My work blends artificial intelligence, real-time control systems, deep learning, data analytics, and robotics to build intelligent autonomous systems that interact with real-world environments.
+                  Now, I'm aiming to leverage the experience I've gathered through my time in Finance, A data science bootcamp and graduate school to make the career change I've been planning for.
+                  The projects I've worked on has led me to the intersection of AI, control systems, data analystics and robotics to create systems that interact with the world around them.
+                  I mainly code in Python, then I learned C++ once I discovered the world of embedded systems. I've also worked with SQL professionally and I know HTML/CSS. I love the freedom C++ allows when it comes to
+                  experimenting and trying new ideas.
+
+                  Outside of work, I enjoy building and tinkering with different robotics related projects. I'm currently in the process of attempting to build my own functioning drone from scratch. Aside from coding, I play the guitar and I'm learning the piano.
+                  I also enjoying reading a variety of generes from mysteries and thrillers, to sci-fi and fantasy as well as nonfiction works. When I'm able to, I enjoy playing basketball and I watch games whenever I can.
+
+
         </p>
       </div>
     </motion.div>
